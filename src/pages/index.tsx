a65ef4ec8b669989a9443ec348b877107e92fb58
Willy-Bueno <willybueno090@gmail.com>
2022-07-04 17:32:49 -0300

    refactor: resolve bug

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,31 @@
 import type { NextPage } from 'next';
+import {
+  BoxStakingBackground,
+  BoxStakingContent,
+  BoxStakingContentHeader,
+  BoxStakingContentInfo,
+  BoxStakingContentForm,
+  BalanceInfo,
+  Button,
+} from '@/components';
 
-const Home: NextPage = () => (
-  <h1>Testing...</h1>
+const Stake: NextPage = () => (
+  <BoxStakingBackground>
+    <div className="w-full bg-gray-600 rounded-lg p-5">
+      <header className="flex items-center justify-center text-2xl font-bold">
+        Staking
+      </header>
+      <BoxStakingContent>
+        <BoxStakingContentHeader />
+        <div className="flex flex-col p-8">
+          <BoxStakingContentInfo />
+          <BoxStakingContentForm />
+          <BalanceInfo />
+          <Button />
+        </div>
+      </BoxStakingContent>
+    </div>
+  </BoxStakingBackground>
 );
 
-export default Home;
+export default Stake;
