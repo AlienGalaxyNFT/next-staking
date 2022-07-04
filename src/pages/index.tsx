4c1a5697eb6276266d4cb700e15461b852ecc007
Willy-Bueno <willybueno090@gmail.com>
2022-07-03 23:40:16 -0300

    chore: add code style airbnb in eslint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,7 @@
-import type { NextPage } from 'next'
+import type { NextPage } from 'next';
 
-const Home: NextPage = () => {
-  return (
-    <h1>Testing...</h1>
-  )
-}
+const Home: NextPage = () => (
+  <h1>Testing...</h1>
+);
 
-export default Home
+export default Home;
