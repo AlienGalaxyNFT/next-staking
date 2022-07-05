128d4c091950f9e96b3c8f970eaa7a7383e7ef2b
Willy-Bueno <willybueno090@gmail.com>
2022-07-05 00:09:17 -0300

    refactor: remove files

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  testPathIgnorePatterns: ["node_modules/", "/.next"],
-  transform: {
-    "^.+\\.(ts|tsx)$": "<rootDir>/node_modules/babel-jest",
-  },
-  setupFilesAfterEnv: ["<rootDir>/tests/setupTests.ts"],
-  testEnvironment: "jsdom",
-};
