1beec18bc6f040f6f6dcb37548c38de445b68921
Alien Galaxy NFT Game <106564800+AlienGalaxyNFT@users.noreply.github.com>
2022-08-24 23:33:32 -0300

    Update addresses.ts

--- src/contracts/addresses.ts
+++ src/contracts/addresses.ts
@@ -1,2 +1,2 @@
-export const tokenAddress = '0x2d933176D1b74F96F3705F5f8713EEB0C45f0fb5';
-export const stakingAddress = '0x08C070F36df9D0eE8C9E1A6e4918425E16A91944';
+export const tokenAddress = '0x8f8eef8EbEb1237D91800BeC63a149D8F9E8e1C6';
+export const stakingAddress = '0xB3f0f297e50F88434142e255B58b5ED7d5610bdb';
